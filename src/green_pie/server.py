409cc92b199558869df68daa0a95c05af11b0f4c
KKrzelowska <kakrzelowska@wp.pl>
2021-06-13 20:48:35 +0200

    iss12: limit data consumption

--- src/green_pie/server.py
+++ src/green_pie/server.py
@@ -10,9 +10,8 @@ def index():
 
 def gen(camera):
     """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
+    frame = camera.get_frame()
+    yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
